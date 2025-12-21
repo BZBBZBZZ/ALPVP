02e68e213c2c5089493b117ba536390632e235d6
Dave Sachio Usanto <dsachiousanto@student.ciputra.ac.id>
2025-12-22 00:07:08 +0700

    fixing main activity dan app routing

--- app/src/main/java/com/example/alpvp/MainActivity.kt
+++ app/src/main/java/com/example/alpvp/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.alpvp.ui.theme.ALPVPTheme
+import com.example.alpvp.ui.route.AppRouting
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +17,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ALPVPTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    AppRouting()
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ALPVPTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/alpvp/ui/route/AppRouting.kt
+++ app/src/main/java/com/example/alpvp/ui/route/AppRouting.kt
@@ -5,14 +5,37 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.alpvp.ui.view.HomeView
+import com.example.alpvp.ui.viewmodel.HomeViewModel
+import com.example.alpvp.ui.view.DetailView
+import com.example.alpvp.ui.viewmodel.DetailViewModel
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
 
 @Composable
 fun AppRouting() {
     val navController = rememberNavController()
+    val homeViewModel: HomeViewModel = viewModel()
+    val detailViewModel: DetailViewModel = viewModel()
 
+    NavHost(navController = navController, startDestination = "home") {
+
+        composable("home") {
+            HomeView(viewModel = homeViewModel, navController = navController)
+        }
+
+        composable(
+            route = "detail/{foodId}",
+            arguments = listOf(navArgument("foodId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val foodId = backStackEntry.arguments?.getInt("foodId") ?: 0
+            DetailView(
+                foodId = foodId,
+                viewModel = detailViewModel,
+                navController = navController
+            )
 
         }
 
         }
     }
-}
